0daeb8cf2f13bd3dbed2ee32f4ff948b4e5269f9
Matt Massoodi <matt@puroxy.io>
2020-11-06 22:29:18 -0800

    minor ideas

--- packages/seed/src/pages/Dashboard.tsx
+++ packages/seed/src/pages/Dashboard.tsx
@@ -1,18 +1,66 @@
 import React, { FC } from 'react'
 import { makeStyles, Theme } from '@material-ui/core/styles'
 import { TopBar } from '../components'
+import { Card, Container, Grid, Typography } from '@material-ui/core'
 
 const useStyles = makeStyles((theme: Theme) => ({
   container: {
     flex: 1,
   },
+  card: {
+    padding: theme.spacing(4),
+  },
 }))
 
 const Dashboard: FC = () => {
+  const { container, card } = useStyles()
   return (
-    <div>
+    <div className={container}>
       <TopBar />
-      Dashboard
+      <Container>
+        <Grid container spacing={2}>
+          {[1, 1, 1].map((tile, i) => (
+            <Grid key={i} item xs={12} sm={4}>
+              <Card className={card}>
+                <Grid container>
+                  <Grid item xs={12}>
+                    0
+                </Grid>
+                  <Grid item xs={12}>
+                    <Typography>Active Orders</Typography>
+                  </Grid>
+                </Grid>
+              </Card>
+            </Grid>
+          ))}
+          {[1, 1].map((tile, i) => (
+            <Grid key={i} item xs={12} sm={6}>
+              <Card className={card}>
+                <Grid container>
+                  <Grid item xs={12}>
+                    0
+                </Grid>
+                  <Grid item xs={12}>
+                    <Typography>Active Orders</Typography>
+                  </Grid>
+                </Grid>
+              </Card>
+            </Grid>
+          ))}
+          <Grid item xs={12}>
+            <Card className={card}>
+              <Grid container>
+                <Grid item xs={12}>
+                  0
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography>Active Orders</Typography>
+                </Grid>
+              </Grid>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   )
 }
--- packages/seed/src/pages/List.tsx
+++ packages/seed/src/pages/List.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from 'react'
 import { makeStyles, Theme } from '@material-ui/core/styles'
+import { TopBar } from '../components'
+import { Container, Grid, Typography } from '@material-ui/core'
 import { RouteComponentProps } from 'react-router-dom'
+import { DataGrid } from '@material-ui/data-grid'
 
 interface IListMatchParams {
   model: string
@@ -12,14 +15,64 @@ const useStyles = makeStyles((theme: Theme) => ({
   container: {
     flex: 1,
   },
+  table: {
+    width: '100%',
+    height: 'calc(100vh - 150px)',
+  },
 }))
 
+const columns = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'firstName', headerName: 'First name', width: 130 },
+  { field: 'lastName', headerName: 'Last name', width: 130 },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 90,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+      `${params.getValue('firstName') || ''} ${
+        params.getValue('lastName') || ''
+      }`,
+  },
+]
+
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+]
+
 const List: FC<IListProps> = ({ match }: IListProps) => {
+  const { container, table } = useStyles()
   const { model } = match.params
   return (
-    <div>
-      {model}
-    </div>
+    <div className={container}>
+      <TopBar />
+      <Container>
+        <Grid item xs={12}>
+          <Typography variant='h4' gutterBottom>
+            {model}
+          </Typography>
+        </Grid>
+        <Grid item className={table} xs={12}>
+          <DataGrid rows={rows} columns={columns} pageSize={10} checkboxSelection />
+        </Grid>
+      </Container>
+    </div >
   )
 }
 
